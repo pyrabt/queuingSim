817922a9d04d6e5c8b2b6cffed10f55b0a00709c
hujianrui <42524052+hujianrui@users.noreply.github.com>
2019-04-08 21:47:59 -0600

    refactoring

--- bankSim.h
+++ bankSim.h
@@ -12,20 +12,24 @@
 
 class bankSim {
 private:
-    std::array<bool, 6> tellers = {false, false, false, false, false, false};
+    std::array<bool, 6> isTellersServicing = {false, false, false, false, false, false};
     std::queue<Event> line;
     std::priority_queue <Event, std::vector<Event>, CompareEvent> eventTiming;
-    std::vector<double> waitingTimes;
+    std::vector<double> allCustomersWaitingTimes;
     long currentTime; //seconds
-    double arrivalRate; //std::stod(argv[1]);
+    double newCustomerArrivalRate; //std::stod(argv[1]);
     int maxCustomerServiceTime; //std::stod(argv[2]);
-    int seed;
+    int seed; //std::stod(argv[3]);
 
     bool isTellerAvailable();
 
     void serveCustomer(Event customer);
 
     void completeService(Event customer);
+    
+    void printResult();
+    
+    void newComeCustomer();
 
 public:
     bankSim(double arrivalRate, double maxServiceTime, int seed);
